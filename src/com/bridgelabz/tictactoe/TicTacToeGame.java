691747164225063caa5b82f9c103f42051975013
rojajullapalli <rojajules@gmail.com>
2021-08-30 08:23:08 +0530

    ADDED: create board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
+
+    public static char[] createBoard() {
+        char[] board = new char[10];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome to tic tac toe problem");
+        createBoard();
     }
 }
