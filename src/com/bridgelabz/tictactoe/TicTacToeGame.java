2705a062a0e4ff5985934b77b2be5cdaa477c591
rojajullapalli <rojajules@gmail.com>
2021-08-30 09:05:27 +0530

    ADDED: User Input Choice

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
     public static char[] createBoard() {
@@ -10,8 +12,33 @@ public class TicTacToeGame {
         return board;
     }
 
+    public static char chooseLetter() {
+        char user;
+        char computer =0 ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. X");
+        System.out.println("2. O");
+        System.out.println("Enter case number to choose letter:- ");
+        int result = sc.nextInt();
+        if (result == 1) {
+            user = 'X';
+            System.out.println("user selects " +user);
+        }
+         else {
+            user = 'O';
+            System.out.println("user selects " +user);
+        }
+        return computer;
+
+}
+
+
     public static void main(String[] args) {
         System.out.println("welcome to tic tac toe problem");
         createBoard();
+        chooseLetter();
+
     }
+
+
 }
